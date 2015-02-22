c00b878125a209e9477e04a85321634171f67e19
Jason P. McLaughlin <jasonpmclaughlin@gmail.com>
2015-02-22 17:06:33 -0600

    major updates to dir structure; added flight plan

--- Vagrantfile
+++ Vagrantfile
@@ -9,7 +9,8 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # \\ Common Settings
 
   # specify box
-  config.vm.box = "hashicorp/precise32"
+  # config.vm.box = "hashicorp/precise32"
+  config.vm.box = "ubuntu/trusty32"
   # specify provision script
   config.vm.provision :shell, :path => "vm_provision/provision-ubuntu-14.04.sh"
   # specify shared folder
@@ -25,16 +26,16 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     master.vm.network "private_network", ip: "55.55.55.5"
   end
 
-  config.vm.define "slave1" do |s1|
-    s1.vm.network "private_network", ip: "66.66.66.6"
-  end
-
-  config.vm.define "slave2" do |s2|
-    s2.vm.network "private_network", ip: "77.77.77.7"
-  end
-
-  config.vm.define "slave3" do |s3|
-    s3.vm.network "private_network", ip: "88.88.88.8"
-  end
+  # config.vm.define "slave1" do |s1|
+  #   s1.vm.network "private_network", ip: "66.66.66.6"
+  # end
+  #
+  # config.vm.define "slave2" do |s2|
+  #   s2.vm.network "private_network", ip: "77.77.77.7"
+  # end
+  #
+  # config.vm.define "slave3" do |s3|
+  #   s3.vm.network "private_network", ip: "88.88.88.8"
+  # end
 
 end
--- vm_provision/provision-ubuntu-14.04.sh
+++ vm_provision/provision-ubuntu-14.04.sh
@@ -4,33 +4,40 @@
 
 # Update Ubuntu
 apt-get update
-# apt-get install -y python-software-properties python g++ make
-# add-apt-repository ppa:chris-lea/node.js
-# apt-get update
-#
+
 # # git
 # echo "----- Provision: Installing git..."
 # apt-get install -y git
 #
-# # Node
-# echo "----- Provision: Installing Node..."
-# apt-get install -y nodejs
-#
-# # npm
-# echo "----- Provision: Installing NPM..."
-# apt-get install -y npm
-#
-# # nginx
-# echo "----- Provision: Installing nginx..."
-# apt-get install -y nginx
-#
+# Node
+echo "----- Provision: Installing Node..."
+apt-get install -y nodejs
+
+# npm
+echo "----- Provision: Installing NPM..."
+apt-get install -y npm
+
+# nginx
+echo "----- Provision: Installing nginx..."
+apt-get install -y nginx
+
+# Install app dependencies
+echo "----- Provision: Installing app dependencies..."
+cd /vagrant
+npm install
+
+# # start app
+# echo "----- Provision: Starting app..."
+# cd /vagrant
+# nodejs ./bin/www
+
 # # forever
 # echo "----- Provision: Installing forever..."
 # npm install -g forever
 #
 # # Cleanup
 # apt-get -y autoremove
-#
+# #
 # # # Restart nginx
 # echo "----- Provision: Starting nginx..."
 # service nginx start
